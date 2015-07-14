325565cff58568208c73c95225e13abea9b59d2b
Brendan Quigley <brendan@beequeue.co.uk>
2015-07-14 01:13:46 +0100

    Further work implementing a ReactJS UI for the table

--- public/assets/jsx/rundown.jsx
+++ public/assets/jsx/rundown.jsx
@@ -12,13 +12,49 @@ var RundownTable = React.createClass({
 
   // Get dataset config and current data
   componentDidMount: function() {
+    var self = this;
     $.ajax({
       url: "api/dataset/" + this.props.dataset,
       success: function(resp) {
-        console.log('setting state');
+        var reducedData = [];
+        var view = resp.config.views[0];
+
+        // Reduce the data down to something more suitable for UI
+        Object.keys(resp.data).forEach(function(key) {
+          var item = resp.data[key].slice(-1)[0];
+          reducedData.push(self.reduceItem(key, item, view.groups));
+        });
+
+        // Sort it by key
+        reducedData.sort(function(a, b) {
+          return a.key == b.key ? 0 : +(a.key > b.key) || -1;
+        });
+
+        // Check for collapsing cells
+        reducedData.map(function(item, rowNum, data) {
+          item.collapses = [];
+
+          // Top row never collapses
+          if (rowNum == 0) {
+            return;
+          }
+
+          // Get previous row for col-by-col comparison
+          prevItem = data[rowNum - 1];
+
+          // If the above cell is the same, we can collapse into it.
+          for (var i = 0; i < item.cols.length; i++) {
+            if (prevItem.cols[i] == item.cols[i]) {
+              item.collapses.push(true);
+            } else {
+              break; // We can't collapse a cell to the right of one that isn't collapsed
+            }
+          }
+        });
+
         this.setState({
-          data: resp.data,
-          view: resp.config.views[0]
+          data: reducedData,
+          view: view
         });
       }.bind(this),
       error: function(xhr, status, err) {
@@ -37,6 +73,25 @@ var RundownTable = React.createClass({
         </table>
       </div>
     );
+  },
+
+  /**
+   * Generate a reduced object suitable for UI
+   * @param  {string} key    The unique key for the row
+   * @param  {Object} item   The individual event to reduce
+   * @param  {Array} groups  The groups (columns) to use
+   * @return {Object}        With props for key and cols
+   */
+  reduceItem: function(key, item, groups) {
+    reduced = {key: key, cols: [], collapses: []};
+    groups.map(function(group) {
+      var val = '';
+      if (item[group.param]) {
+        val = item[group.param]
+      }
+      reduced.cols.push(val);
+    });
+    return reduced;
   }
 });
 
@@ -45,7 +100,6 @@ var RundownTable = React.createClass({
 var RundownTableHead = React.createClass({
 
   render: function() {
-    console.log('rendering head');
     if (!this.props.view.groups) {
       return (<thead/>);
     }
@@ -67,10 +121,21 @@ var RundownTableHead = React.createClass({
 //--------------------------------------------------------------------------
 
 var RundownTableBody = React.createClass({
+  getInitialState: function() {
+    return {data: [], view: {}};
+  },
   render: function() {
+    if (!this.props.data.length) {
+      return (<tbody/>);
+    }
+    var trNodes = this.props.data.map(function(item) {
+      return (
+        <RundownTableRow key={item.key} item={item} />
+      );
+    });
     return (
       <tbody>
-        <tr><td>Body</td></tr>
+        {trNodes}
       </tbody>
     );
   }
@@ -79,13 +144,27 @@ var RundownTableBody = React.createClass({
 //--------------------------------------------------------------------------
 
 var RundownTableRow = React.createClass({
-  render: function() {
+
+ render: function() {
+    var item = this.props.item;
+    var tdNodes = item.cols.map(function(val, i) {
+      // Is this column collapsed to the one above?
+      var cssClass = '';
+      if (item.collapses[i]) {
+        console.log(item);
+        cssClass = 'collapsed';
+      }
+      return (
+        <td key={i} className={cssClass}>{val}</td>
+      );
+    });
     return (
-      <tbody>
-        <tr><td>Body</td></tr>
-      </tbody>
+      <tr>
+        {tdNodes}
+      </tr>
     );
   }
+
 });
 
 
